98ff47872e1df6574d455827c11e7d0f292a86ba
Randy Huang <huangyulu.zh@gmail.com>
2024-01-13 10:53:21 -0500

    remove unnecessary

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Login from './Authorization/Login';
+
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Dashboard from './Dashboard/Dashboard';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
